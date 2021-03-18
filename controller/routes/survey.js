42d99d7def9706a81b3e723a9f4d12951d355840
benl95 <beno_1995@hotmail.com>
2021-03-18 16:08:58 +0100

    feat: create radio button for each item in array

--- controller/routes/survey.js
+++ controller/routes/survey.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
 
+const names = [
+	'Vasilis van Gemert',
+	'Justus Sturkenboom',
+	'Guido Bouwman',
+	'Peter Paul Koch',
+	'Suus ten Voorde',
+	'Joost Faber',
+	'Sanne t Hooft',
+	'Declan Rek',
+	'Koop Reynders',
+];
+
 router.get('/', (req, res) => {
 	res.render('form', {
 		title: 'Enquete',
+		teachers: names,
 	});
 });
 
