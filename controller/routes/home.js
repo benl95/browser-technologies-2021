f81633bcb25edd460409be7d70b04767191caf12
benl95 <beno_1995@hotmail.com>
2021-03-25 16:26:53 +0100

    feat: render complete/incomplete surveys

--- controller/routes/home.js
+++ controller/routes/home.js
@@ -1,9 +1,22 @@
 const router = require('express').Router();
+const {
+	getDataFile,
+	parseData,
+	findUserSurveys,
+	getCompleteStatus,
+} = require('../../data/helpers/surveyData');
 
 router.get('/home/:id', (req, res) => {
+	const id = req.params.id;
+	const file = getDataFile();
+	const data = parseData(file);
+	const userSurveys = findUserSurveys(data, id);
+	const surveys = getCompleteStatus(userSurveys);
+
 	res.render('home', {
 		title: 'Minor Web Design & Development',
 		id: req.params.id,
+		data: surveys,
 	});
 });
 
