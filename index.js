40ef393c66f0616396901a625e2f220a0a22b8fe
benl95 <beno_1995@hotmail.com>
2021-03-25 10:12:25 +0100

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 // Require routes
 const login = require('./controller/routes/login.js');
